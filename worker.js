f4880bf8a2f7c96842fc7b450adc2c1ba5949109
Huan Nguyen <hpn007@ucsd.edu>
2018-12-08 08:45:59 -0800

    Added Dummy Service Worker

---
+++ worker.js
@@ -0,0 +1,14 @@
+console.log("(Worker) Hello World!")
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+);
